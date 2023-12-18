7a57cf84e7fd80e0e1035c8853dcf103c27f2064
Tanushree Rai <raitanushree2017@gmail.com>
2023-12-18 17:43:17 +0530

    first

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,13 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.split("").sort().join("")==str2.split("").sort().join("")){
+    return true;
+  }
+  return false;
 }
+
+var ans=isAnagram("code", "edoc");
+console.log(ans);
 
 module.exports = isAnagram;
